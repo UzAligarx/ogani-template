022f63bae849e2f7c0694a9799957d9f33398ea0
UzAligarx <izzatbektoshboyev7959@gmail.com>
2022-08-19 01:18:27 +0500

    second

--- resources/views/frontend/home-page/index.blade.php
+++ resources/views/frontend/home-page/index.blade.php
@@ -10,6 +10,7 @@
                 <div class="col-lg-9 offset-lg-3">
                     <div class="hero__item set-bg" data-setbg="{{ asset('storage/hero/banner') }}.jpg">
                         <div class="hero__text">
+                            
                             <span>FRUIT FRESH</span>
                             <h2>Vegetable <br />100% Organic</h2>
                             <p>Free Pickup and Delivery Available</p>
